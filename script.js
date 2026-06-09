aa9f76583a656da1e91e41a1589047c9b3aa765f
muhammed-ahmed877 <ahmeddehware@gmail.com>
2026-06-10 01:05:18 +0500

    kkk

--- script.js
+++ script.js
@@ -46,29 +46,3 @@ function edittodo(index) {
 }
 
 
-// function showTask() {
-//   list.innerHTML = "";
-//   for (let i = 0; i < tasks.length; i++) {
-//     list.innerHTML += `<li>${tasks[i]}
-//     <button onClick= "deleteTodo(${i})" >Delete</button>
-//     <button onClick= "editTodo(${i})">Edit</button>
-    
-//     </li>`;
-//   }
-// }
-
-// function deleteTodo(index){
-
-
-//     tasks.splice(index , 1)
-//     console.log('deleted')
-
-//     showTask()
-// }
-
-// function editTodo(index){
-//     let newTask = prompt('Enter a task')
-//     tasks[index] = newTask
-//     console.log(tasks)
-//     showTask()
-// }
