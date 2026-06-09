b550f343c2e3cca571c0c100d077fcfd3561ea88
muhammed-ahmed877 <ahmeddehware@gmail.com>
2026-06-10 00:59:43 +0500

    done

--- script.js
+++ script.js
@@ -1,41 +1,50 @@
+let todo = document.querySelector("#input")
+let btn = document.querySelector("#btn")
+let list = document.querySelector("#list")
 
+let task =[]
 
+function addtask () {
+ if (todo.value == "") {
+  alert("kindly enter some thing")
+  
+ } else{
+  task.push(todo.value)
+  console.log(task);
+    
+ } 
+ todo.value=""; 
 
+  showTask();
+}
 
+function showTask() {
+  list.innerHTML="";
+  for (let i = 0; i < task.length; i++) {
+    list.innerHTML +=`<li>${task[i]}
+    <button onclick="deletetodo(${i})">delete todo</button>
+    <button onclick="edittodo(${i})">edit todo</button>
+    </li>`
+  }
+  
+}
 
+function deletetodo(index) {
+  task.splice(index ,1)
+  console.log("todo deletde");
+  showTask();
 
+}
+ 
+function edittodo(index) {
+  let newtodo= prompt("enter the new todo")
+  task[index]= newtodo
+  console.log(task);
+  showTask()
+  
+  
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let todo = document.querySelector("#taskInput");
-// let btn = document.querySelector("#btn");
-
-// let list = document.querySelector("#list");
-
-// let tasks = [];
-
-// function addTask() {
-//   if (todo.value == "") {
-//     alert("please fill the field");
-//   } else {
-//     tasks.push(todo.value);
-//     console.log(tasks);
-//   }
-//   todo.value = "";
-
-//   showTask();
-// }
 
 // function showTask() {
 //   list.innerHTML = "";
